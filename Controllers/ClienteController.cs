c90e124be505ef206da581cb55448b77c7bfb58c
CrisAce12 <crisace52@gmail.com>
2021-05-14 19:34:28 -0500

    Creación de plantilla Cliente.

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto.Models;
+
+namespace Proyecto.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+
+            using (var db = new inventarioEntities1())
+            {
+
+                return View(db.cliente.ToList());
+
+            }
+                
+        }
+    }
+}
